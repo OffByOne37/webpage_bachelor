368af64cfc661d08acc0de8a298b0c5eeee95a4e
jonasTscholl <79969107+jonasTscholl@users.noreply.github.com>
2023-05-31 11:49:19 +0200

    Updated Header with Routing

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
