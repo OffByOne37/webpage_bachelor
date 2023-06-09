5249f27ded0a3fba122ee70946f41fbb1e8a5e98
jonasTscholl <79969107+jonasTscholl@users.noreply.github.com>
2023-06-09 14:50:10 +0200

    wip

--- src/components/Parameter/Number/Editors/SpecificEditor.js
+++ src/components/Parameter/Number/Editors/SpecificEditor.js
@@ -2,6 +2,7 @@ import React from "react";
 import { EditorField } from "../NumberParameterBlock";
 import SimpleIntegerEditor from "./SimpleIntegerEditor";
 import ColorEditor from "./ColorEditor";
+import NoteEditor from "./NoteEditor";
 
 
 const SpecificEditor = ({ parameter, editorField, handlePropertyChange }) => {
@@ -13,7 +14,11 @@ const SpecificEditor = ({ parameter, editorField, handlePropertyChange }) => {
         case EditorField.Color:
             return (
             <ColorEditor parameter={parameter} handlePropertyChange={handlePropertyChange}/>
-            )
+            );
+        case EditorField.Note:
+            return (
+            <NoteEditor parameter={parameter} handlePropertyChange={handlePropertyChange}/>
+            );
         default:
             console.log("Default case");
             return null;
