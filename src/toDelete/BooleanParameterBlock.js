c8cb45434b58119a606da1da198b13a4f02b1316
jonasTscholl <79969107+jonasTscholl@users.noreply.github.com>
2023-09-06 10:59:20 +0200

    ordered files to delete

---
+++ src/toDelete/BooleanParameterBlock.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "../Parameter.css";
+import ParameterBlock from '../ParameterBlock';
+import "../PictureEditor.css";
+import { PossibleBoolEditors } from '../components/Parameter/Boolean/PossibleBoolEditors';
+
+const BooleanParameterBlock = ({ parameter, handlePropertyChangeBoolean }) => {
+
+    const boolEditorOptions = [
+        {
+            text: "Use toggle switch",
+            editor: PossibleBoolEditors.Toggle
+        },
+        {
+            text: "Use variable as input",
+            editor: PossibleBoolEditors.Variable
+        }
+    ]
+
+
+    return (
+        <ParameterBlock parameter={parameter} handlePropertyChange={handlePropertyChangeBoolean} startEditor={PossibleBoolEditors.None} options={boolEditorOptions}/>
+    )
+}
+export default BooleanParameterBlock;
