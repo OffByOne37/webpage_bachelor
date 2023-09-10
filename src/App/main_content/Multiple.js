bf0c1bc65954767e7cec99fd02bf22022f95d172
jonasTscholl <79969107+jonasTscholl@users.noreply.github.com>
2023-09-10 15:18:09 +0200

    more or less finished multiple page

--- src/App/main_content/Multiple.js
+++ src/App/main_content/Multiple.js
@@ -19,12 +19,15 @@ const Multiple = () => {
   );
   const [currFunction, setCurrFunction] = useState(null);
   const [functions, setFunctions] = useState({});
-  const [namespace, setNamespace] = useState("not_defined");
+  const [namespace, setNamespace] = useState("not defined");
+  const [namespaceColor, setNamespaceColor] = useState("#aabbcc");
+  const [namespaceIcon, setNamespaceIcon] = useState(undefined);
+  const [namespaceWeight, setNamespaceWeigth] = useState(undefined);
 
   const [sizes, setSizes] = useState(["20%", "40%", "22%", "3%", "15%"]);
 
   const updateFunctionCode = (code) => {
-    updateCodeAndParameter(updateAttribute, code, functions[currFunction])
+    updateCodeAndParameter(updateAttribute, code, functions[currFunction]);
   };
 
   const updateAttribute = (attribute, newValue) => {
@@ -66,38 +69,70 @@ const Multiple = () => {
     let allFinalFunction = "";
     let groupSet = new Set();
     let hasError = false; // Initialize a flag to track errors
-  
+
     Object.keys(functions).forEach((key) => {
       if (hasError) return; // If an error has already occurred, exit the loop
-  
+
       let currGroup = functions[key].group;
       if (currGroup) {
         groupSet.add(currGroup);
       }
       let curGenerated = generateCodeForFunction(functions[key]);
-  
+
       if (curGenerated == null) {
         //In the case that there was an error
         hasError = true; // Set the flag to true when an error occurs
         return; // Exit the loop when an error occurs
       }
-  
+
       allFinalFunction += curGenerated + "\n\n";
     });
-  
+
     if (hasError) return; // If an error occurred, exit the function
-  
+
+    const namespaceInfo = generateNamespaceInfo(groupSet);
+
+    // let namespaceInfo =
+    //   "//% groups='[" +
+    //   Array.from(groupSet)
+    //     .map((item) => `"${item}"`)
+    //     .join(", ") +
+    //   "]'\n";
+
+    // namespaceInfo += "//% block=\""+ namespace +"\"\n"
+
+    // namespaceInfo += "//% color=" + namespaceColor + "\n";
+    // namespaceInfo +=
+    //   namespaceIcon !== undefined
+    //   ? '//% icon="\\u' + namespaceIcon + '"\n'
+    //   : "";
+
+    // namespaceInfo +=
+    //   namespaceWeight !== undefined
+    //     ? '//% weight="' + namespaceWeight + '"\n'
+    //     : "";
+
     setFinalFunction(
-      "//% groups='[" +
-        Array.from(groupSet)
-          .map((item) => `"${item}"`)
-          .join(", ") +
-        "]'\n" +
-        "namespace " + namespace + "{\n" +
-        allFinalFunction + "}"
+      namespaceInfo +
+        "namespace " +
+        namespace.replace(/\s/g, "") +
+        "{\n" +
+        allFinalFunction +
+        "}"
     );
   };
-  
+
+  const generateNamespaceInfo = (groupSet) => {
+    const groups = Array.from(groupSet)
+      .map((item) => `"${item}"`)
+      .join(", ");
+    const icon =
+      namespaceIcon !== undefined ? `//% icon="\\u${namespaceIcon}"\n` : "";
+    const weight =
+      namespaceWeight !== undefined ? `//% weight="${namespaceWeight}"\n` : "";
+
+    return `//% groups='[${groups}]'\n//% block="${namespace}"\n//% color=${namespaceColor}\n${icon}${weight}`;
+  };
 
   return (
     <div className="main-main-content">
@@ -116,6 +151,12 @@ const Multiple = () => {
             setCurrFunction={setCurrFunction}
             namespace={namespace}
             setNamespace={setNamespace}
+            namespaceColor={namespaceColor}
+            setNamespaceColor={setNamespaceColor}
+            namespaceIcon={namespaceIcon}
+            setNamespaceIcon={setNamespaceIcon}
+            namespaceWeight={namespaceWeight}
+            setNamespaceWeigth={setNamespaceWeigth}
           />
         </Pane>
         <Pane
--- src/App/main_content/components/FunctionContainer.js
+++ src/App/main_content/components/FunctionContainer.js
@@ -7,6 +7,8 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./css/Functions.css";
 import { defaultFunction } from "./DefaultFunction";
+import { HexColorPicker } from "react-colorful";
+import HelpInformation from "./HelpInformation";
 
 const FunctionContainer = ({
   functions,
@@ -15,6 +17,12 @@ const FunctionContainer = ({
   setCurrFunction,
   namespace,
   setNamespace,
+  namespaceColor,
+  setNamespaceColor,
+  namespaceIcon,
+  setNamespaceIcon,
+  namespaceWeight,
+  setNamespaceWeigth,
 }) => {
   const [showPopup, setShowPopup] = useState(false);
   const [inputText, setInputText] = useState("");
@@ -54,24 +62,78 @@ const FunctionContainer = ({
     setCurrFunction(functionName);
   };
 
+  const handleColorChange = (color) => {
+    setNamespaceColor(color);
+  };
+
+  const handleIconChange = (iconCode) => {
+    let newValue = iconCode.trim();
+    setNamespaceIcon(newValue === "" ? undefined : newValue);
+  };
+
+  const handleWeightChange = (weight) => {
+    let newValue = weight.replace(/\D/g, "");
+    setNamespaceWeigth(newValue === "" ? undefined : newValue);
+  };
+
   return (
     <div className="function-window">
-      <h5>Please enter the namespace-name:</h5>
+      <h5>
+        Please enter the namespace-name
+        <HelpInformation help="This will be the name visible in the codeEditor." />
+        :
+      </h5>
       <input
         type="text"
         value={namespace}
         onChange={(e) => setNamespace(e.target.value)}
         required={showPopup}
       />
+      <h5>
+        Please select the namespace color
+        <HelpInformation help="Pick color for the namespace. All blocks within the same namespace have the same color so that users can find the category easily from samples." />
+        :
+      </h5>
+      <HexColorPicker color={namespaceColor} onChange={handleColorChange} />
+      <h5>
+        Please select the namespace icon
+        <HelpInformation
+          help={
+            "Any free non-brand icon from Font Awesome (v5.15.4 at the time of writing) can be used. The full list can be found https://fontawesome.com/v5/search?m=free"
+          }
+        />
+        :
+      </h5>
+      <input
+        type="text"
+        placeholder="Enter Unicode value"
+        value={namespaceIcon}
+        onChange={(e) => handleIconChange(e.target.value)}
+      />
+
+      <h5>
+        Please enter namespace weight
+        <HelpInformation
+          help={
+            "determines where your category appears in the toolbox. Higher weight means it appears closer to the top"
+          }
+        />
+        :
+      </h5>
+      <input
+        type="text"
+        placeholder="Enter weight"
+        value={namespaceWeight}
+        onChange={(e) => handleWeightChange(e.target.value)}
+      />
+
       <h5>Add your functions here:</h5>
       <div className="file-explorer">
         {Object.keys(functions).map((functionName) => (
           <div
             key={functionName}
             className={`file-item ${
-              currFunction && currFunction === functionName
-                ? "highlighted"
-                : ""
+              currFunction && currFunction === functionName ? "highlighted" : ""
             }`}
             onClick={() => handleFunctionClick(functionName)}
           >
