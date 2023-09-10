5fb6c468e9091e7327c3ac69bf30cebe15eab823
jonasTscholl <79969107+jonasTscholl@users.noreply.github.com>
2023-09-10 15:27:04 +0200

    updated order of header

--- src/App/sections/Header.js
+++ src/App/sections/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   MDBContainer,
   MDBNavbar,
@@ -8,56 +8,64 @@ import {
   MDBNavbarNav,
   MDBNavbarItem,
   MDBCollapse,
-} from 'mdb-react-ui-kit';
-import { Link } from 'react-router-dom';
-import "./css/Header.css"
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+import "./css/Header.css";
 
 const Header = () => {
   const [showBasic, setShowBasic] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' style={{backgroundColor: '#003361', height: "56px"}}>
+    <MDBNavbar
+      expand="lg"
+      style={{ backgroundColor: "#003361", height: "56px" }}
+    >
       <MDBContainer fluid>
-        <MDBNavbarBrand href='#' className='mb-0 h1' style={{color: 'rgb(243,146,0)'}}>MakeCode Extension Development Tool</MDBNavbarBrand>
+        <MDBNavbarBrand
+          href="#"
+          className="mb-0 h1"
+          style={{ color: "rgb(243,146,0)" }}
+        >
+          MakeCode Extension Development Tool
+        </MDBNavbarBrand>
 
         <MDBNavbarToggler
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
           onClick={() => setShowBasic(!showBasic)}
-          style={{color:'rgb(243,146,0)'}}
+          style={{ color: "rgb(243,146,0)" }}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon="bars" fas />
         </MDBNavbarToggler>
 
         <MDBCollapse navbar show={showBasic}>
-          <MDBNavbarNav className='justify-content-end mb-lg-0'>
+          <MDBNavbarNav className="justify-content-end mb-lg-0">
             <MDBNavbarItem>
-              <Link active to='/' className='nav-link'>
+              <Link active to="/" className="nav-link">
                 Home
               </Link>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <Link active to='/single' className='nav-link'>
+              <Link active to="/single" className="nav-link">
                 Create Function
               </Link>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <Link active to="/enum" className='nav-link'>
-                Create Enum
-              </Link>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <Link active to="/multiple" className='nav-link'>
+              <Link active to="/multiple" className="nav-link">
                 Create File
               </Link>
             </MDBNavbarItem>
-            </MDBNavbarNav>
-
+            <MDBNavbarItem>
+              <Link active to="/enum" className="nav-link">
+                Create Enum
+              </Link>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
         </MDBCollapse>
       </MDBContainer>
     </MDBNavbar>
   );
-}
+};
 
 export default Header;
